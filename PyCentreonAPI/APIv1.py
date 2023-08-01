2c31905206cc03831bcf21990410f430e71fa5f1
Denis Lamačka <43756894+locadm@users.noreply.github.com>
2023-08-01 08:51:36 +0200

    Update APIv1.py
    get all contacts
    set host param
    rename service
    add service
    set service param
    set service macro
    get service macro
    show services

--- PyCentreonAPI/APIv1.py
+++ PyCentreonAPI/APIv1.py
@@ -35,8 +35,8 @@ def authenticate(url: str, username: str, password: str) -> str:
     v1_server_url = url
     return token
 
-
-def show_services(host=None, service=None):
+#todo: finish
+def get_hostgroup(host: str = None, service: str = None):
     global v1_api_token, v1_server_url
     check_token()
 
@@ -55,4 +55,151 @@ def show_services(host=None, service=None):
             service = ""
         payload["values"] = ';'.join([host, service])
     return requests.post("{}/centreon/api/index.php?action=action&object=centreon_clapi".format(v1_server_url),
-                         data=json.dumps(payload), headers=c_header).json()["result"]
+                         data=json.dumps(payload), headers=c_header).json()
+
+
+def get_services(host: str = None, service: str = None):
+    global v1_api_token, v1_server_url
+    check_token()
+
+    c_header = {
+        "Content-Type": "application/json",
+        "centreon-auth-token": v1_api_token
+    }
+    payload = {
+        "action": "show",
+        "object": "SERVICE",
+    }
+    if host is not None or service is not None:
+        if host is None:
+            host = ""
+        if service is None:
+            service = ""
+        payload["values"] = ';'.join([host, service])
+    return requests.post("{}/centreon/api/index.php?action=action&object=centreon_clapi".format(v1_server_url),
+                         data=json.dumps(payload), headers=c_header).json()
+
+
+def get_service_macro(host: str, service: str):
+    global v1_api_token, v1_server_url
+    check_token()
+
+    c_header = {
+        "Content-Type": "application/json",
+        "centreon-auth-token": v1_api_token
+    }
+    payload = {
+        "action": "getmacro",
+        "object": "SERVICE",
+        "values": "{host};{service}".format(host=host, service=service)
+    }
+
+    return requests.post("{}/centreon/api/index.php?action=action&object=centreon_clapi".format(v1_server_url),
+                         data=json.dumps(payload), headers=c_header).json()
+
+
+def set_service_macro(host: str, service: str, macro_name: str, macro_value: str, macro_description: str, is_password: bool = False):
+    global v1_api_token, v1_server_url
+    check_token()
+
+    c_header = {
+        "Content-Type": "application/json",
+        "centreon-auth-token": v1_api_token
+    }
+    payload = {
+        "action": "setmacro",
+        "object": "SERVICE",
+        "values": "{host};{service};{mname};{mvalue};{ispwd};{descr}".format(host=host, service=service, mname=macro_name, mvalue=macro_value, ispwd=int(is_password), descr=macro_description)
+    }
+
+    return requests.post("{}/centreon/api/index.php?action=action&object=centreon_clapi".format(v1_server_url),
+                         data=json.dumps(payload), headers=c_header).json()
+
+
+def set_service_param(host: str, service: str, parameter: str, value: str):
+    global v1_api_token, v1_server_url
+    check_token()
+
+    c_header = {
+        "Content-Type": "application/json",
+        "centreon-auth-token": v1_api_token
+    }
+    payload = {
+        "action": "setparam",
+        "object": "SERVICE",
+        "values": "{host};{service};{param};{val}".format(host=host, service=service, param=parameter, val=value)
+    }
+
+    return requests.post("{}/centreon/api/index.php?action=action&object=centreon_clapi".format(v1_server_url),
+                         data=json.dumps(payload), headers=c_header).json()
+
+
+def add_service(host: str, service: str, service_template: str):
+    global v1_api_token, v1_server_url
+    check_token()
+
+    c_header = {
+        "Content-Type": "application/json",
+        "centreon-auth-token": v1_api_token
+    }
+    payload = {
+        "action": "add",
+        "object": "SERVICE",
+        "values": "{host};{service};{template}".format(host=host, service=service, template=service_template)
+    }
+
+    return requests.post("{}/centreon/api/index.php?action=action&object=centreon_clapi".format(v1_server_url),
+                         data=json.dumps(payload), headers=c_header).json()
+
+
+def rename_service(host: str, old_name: str, new_name: str):
+    global v1_api_token, v1_server_url
+    check_token()
+
+    c_header = {
+        "Content-Type": "application/json",
+        "centreon-auth-token": v1_api_token
+    }
+    payload = {
+        "action": "setparam",
+        "object": "SERVICE",
+        "values": "{host};{old_name};description;{new_name}".format(host=host, old_name=old_name, new_name=new_name)
+    }
+
+    return requests.post("{}/centreon/api/index.php?action=action&object=centreon_clapi".format(v1_server_url),
+                         data=json.dumps(payload), headers=c_header).json()
+
+
+def set_host_param(host: str, parameter: str, value: str):
+    global v1_api_token, v1_server_url
+    check_token()
+
+    c_header = {
+        "Content-Type": "application/json",
+        "centreon-auth-token": v1_api_token
+    }
+    payload = {
+        "action": "setparam",
+        "object": "HOST",
+        "values": "{host};{param};{val}".format(host=host, param=parameter, val=value)
+    }
+
+    return requests.post("{}/centreon/api/index.php?action=action&object=centreon_clapi".format(v1_server_url),
+                         data=json.dumps(payload), headers=c_header).json()
+
+
+def get_all_contacts():
+    global v1_api_token, v1_server_url
+    check_token()
+
+    c_header = {
+        "Content-Type": "application/json",
+        "centreon-auth-token": v1_api_token
+    }
+    payload = {
+        "action": "show",
+        "object": "CONTACT"
+    }
+
+    return requests.post("{}/centreon/api/index.php?action=action&object=centreon_clapi".format(v1_server_url),
+                         data=json.dumps(payload), headers=c_header).json()
